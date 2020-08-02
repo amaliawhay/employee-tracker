e7afc30cfc7e607bd83d2b1cbbb9069acd8cc9d0
Amalia Hay <amaliawhay@gmail.com>
2020-08-02 14:21:20 -0700

    Schema and connection

---
+++ db/schema.sql
@@ -0,0 +1,41 @@
+###
+Schema
+CREATE DATABASE cms_db;
+USE cms_db;
+
+CREATE TABLE department
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+	name varchar
+    (30) NOT NULL,
+	PRIMARY KEY
+    (id)
+);
+
+    CREATE TABLE role
+    (
+        id int NOT NULL
+        AUTO_INCREMENT,
+	title varchar
+        (30) NOT NULL,
+	salary DECIMAL NOT NULL,
+	department_id int NOT NULL,
+	PRIMARY KEY
+        (id)
+);
+        CREATE TABLE employee
+        (
+            id int NOT NULL
+            AUTO_INCREMENT,
+	first_name varchar
+            (30) NOT NULL,
+        last_name varchar
+            (30) NOT NULL,
+	role_id int NOT NULL,
+    manager_id int,
+	PRIMARY KEY
+            (id)
+);
+
+
